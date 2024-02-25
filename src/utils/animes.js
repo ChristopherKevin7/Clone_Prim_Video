501554eb01d7319f3334b35044c47c3eeb2dca35
Christopher Kevin Costa <christopherkevin78@gmail.com>
2024-02-25 19:30:33 -0300

    first commit

---
+++ src/utils/animes.js
@@ -0,0 +1,17 @@
+export const ANIMES = [
+    {
+        id: "1",
+        name: "One Piece Red",
+        moviesURL: require("../assets/movies/OnePieceRed.png"),
+      },
+      {
+        id: "2",
+        name: "Demon Slayer",
+        moviesURL: require("../assets/movies/DemonSlayer.jpg"),
+      },
+      {
+        id: "3",
+        name: "Evangelion",
+        moviesURL: require("../assets/movies/Evangelion.jpg"),
+      },
+]
